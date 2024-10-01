a59033ca4a6c25e6b5e0d347a31c9297358c150e
SeppDev <70803492+SeppDev@users.noreply.github.com>
2024-10-01 16:12:19 +0200

    update

--- src/analyzer/types.rs
+++ src/analyzer/types.rs
@@ -1,1 +1,27 @@
-pub enum CustomType {}
+use std::collections::HashMap;
+
+use crate::parser::Path;
+
+pub struct Types {
+    pub function: HashMap<Path, Function>
+}
+impl Types {
+    pub fn new() -> Self {
+        Self {
+            functions: HashMap::new(),
+        }
+    }
+    pub fn get(&mut self) {
+
+    } 
+}
+pub struct Function {
+
+}
+
+pub struct Enum {
+
+}
+pub struct Struct {
+
+}
