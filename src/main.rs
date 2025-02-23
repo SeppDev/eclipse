a142045fda6a2bc2f5873977d273d3d97186ddb8
SeppDev <seppdemeijer@gmail.com>
2025-02-23 22:57:38 +0100

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod cli;
+mod commands;
+mod common;
+mod compiler;
+mod diagnostics;
+mod utils;
+
+pub const FILE_EXTENSION: &str = "ecl";
+
+fn main() {
+    cli::CLI::new()
+        .register(
+            "analyzes current project",
+            vec!["c", "check"],
+            commands::check,
+        )
+        .register(
+            "Builds current project",
+            vec!["b", "build"],
+            commands::build,
+        )
+        .register(
+            "Build and runs current project",
+            vec!["r", "run"],
+            commands::run,
+        )
+        .register(
+            "Create a new project <path>",
+            vec!["n", "new"],
+            commands::new,
+        )
+        .start();
+}
